9e67800eb29cb458357a13a6da12165695d29b7a
alex90bar <aleksandr.baranov@skillbox.ru>
2022-06-02 11:51:02 +0300

    init

---
+++ src/test/java/com/alex90bar/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex90bar.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
